6f9e9dae481baf0521b4914112abcb1878b58914
Juan Rodriguez <juanrd@iMac-de-Juan.local>
2021-02-12 01:10:58 +0100

    random ia implemented

--- project_web/ex00/myscript.js
+++ project_web/ex00/myscript.js
@@ -44,38 +44,107 @@ function checkWinner(x, y) {
 	return 2;
 }
 
+function play1_vs_1(){
+	document.querySelectorAll('td').forEach(item => {
+		item.addEventListener('click', event => {
+			coordinates = item.id.split('.');
+			if (array[coordinates[0]][coordinates[1]] == 0 && click) {
+				array[coordinates[0]][coordinates[1]] = turn;
+				item.innerHTML += "<p id='clicked'>" + turn + "</p>"
+				if (checkWinner(coordinates[0], coordinates[1]) == 1) {
+					document.querySelector('h1').removeAttribute('hidden');
+					document.getElementById('winner').innerHTML = turn;
+					document.querySelector('p').setAttribute('hidden', 'true');
+					document.getElementById('retry').removeAttribute('hidden');
+					click = false;
+				} else if (checkWinner(coordinates[0], coordinates[1]) == 2) {
+					document.querySelector('h1').removeAttribute('hidden');
+					document.getElementById('winner').innerHTML = "none";
+					document.querySelector('p').setAttribute('hidden', 'true');
+					document.getElementById('retry').removeAttribute('hidden');
+					click = false;
+				}
+				turn = turn == 'X' ? 'O' : 'X';
+				document.getElementById('turn').innerHTML = turn;
+			}
+		});
+	});
+}
+
+function ia_movement(){
+	rand1 = 1
+	rand2 = 1
+	while (array[rand1][rand2] != 0){
+		rand1 = Math.floor(Math.random() * 3)
+		rand2 = Math.floor(Math.random() * 3)
+	}
+	item = rand1 + "." + rand2;
+	document.getElementById(item).innerHTML = "<p id='clicked'>O</p>"
+	array[rand1][rand2] = turn;
+	if (checkWinner(rand1, rand2) == 1) {
+		document.querySelector('h1').removeAttribute('hidden');
+		document.getElementById('winner').innerHTML = turn;
+		document.querySelector('p').setAttribute('hidden', 'true');
+		document.getElementById('retry').removeAttribute('hidden');
+		click = false;
+	} else if (checkWinner(rand1, rand2) == 2) {
+		document.querySelector('h1').removeAttribute('hidden');
+		document.getElementById('winner').innerHTML = "none";
+		document.querySelector('p').setAttribute('hidden', 'true');
+		document.getElementById('retry').removeAttribute('hidden');
+		click = false;
+	}
+	turn = 'X';
+}
+
+function play_ia(){
+	document.querySelectorAll('td').forEach(item => {
+		item.addEventListener('click', event => {
+			coordinates = item.id.split('.');
+			if (array[coordinates[0]][coordinates[1]] == 0 && click) {
+				array[coordinates[0]][coordinates[1]] = turn;
+				item.innerHTML += "<p id='clicked'>X</p>"
+				if (checkWinner(coordinates[0], coordinates[1]) == 1) {
+					document.querySelector('h1').removeAttribute('hidden');
+					document.getElementById('winner').innerHTML = turn;
+					document.querySelector('p').setAttribute('hidden', 'true');
+					document.getElementById('retry').removeAttribute('hidden');
+					click = false;
+				} else if (checkWinner(coordinates[0], coordinates[1]) == 2) {
+					document.querySelector('h1').removeAttribute('hidden');
+					document.getElementById('winner').innerHTML = "none";
+					document.querySelector('p').setAttribute('hidden', 'true');
+					document.getElementById('retry').removeAttribute('hidden');
+					click = false;
+				} else {
+				turn = 'O';
+				ia_movement();
+				}
+			}
+		});
+	});
+}
+
 document.getElementById('play').addEventListener('click', event => {
 	document.getElementById('play').setAttribute('hidden', 'true');
+	document.getElementById('play_ia').setAttribute('hidden', 'true');
 	document.querySelector('table').removeAttribute('hidden');
 	document.getElementById('turn').innerHTML = turn;
 	document.querySelector('p').removeAttribute('hidden');
+	play1_vs_1();
 });
+
+document.getElementById('play_ia').addEventListener('click', event => {
+	document.getElementById('play').setAttribute('hidden', 'true');
+	document.getElementById('play_ia').setAttribute('hidden', 'true');
+	document.querySelector('table').removeAttribute('hidden');
+	document.getElementById('turn').innerHTML = turn;
+	document.querySelector('p').removeAttribute('hidden');
+	play_ia();
+});
+
 
 document.getElementById('retry').addEventListener('click', event => {
 	location.reload();
 });
 
-document.querySelectorAll('td').forEach(item => {
-	item.addEventListener('click', event => {
-		coordinates = item.id.split('.');
-		if (array[coordinates[0]][coordinates[1]] == 0 && click) {
-			array[coordinates[0]][coordinates[1]] = turn;
-			item.innerHTML += "<p id='clicked'>" + turn + "</p>"
-			if (checkWinner(coordinates[0], coordinates[1]) == 1) {
-				document.querySelector('h1').removeAttribute('hidden');
-				document.getElementById('winner').innerHTML = turn;
-				document.querySelector('p').setAttribute('hidden', 'true');
-				document.getElementById('retry').removeAttribute('hidden');
-				click = false;
-			} else if (checkWinner(coordinates[0], coordinates[1]) == 2) {
-				document.querySelector('h1').removeAttribute('hidden');
-				document.getElementById('winner').innerHTML = "none";
-				document.querySelector('p').setAttribute('hidden', 'true');
-				document.getElementById('retry').removeAttribute('hidden');
-				click = false;
-			}
-			turn = turn == 'X' ? 'O' : 'X';
-			document.getElementById('turn').innerHTML = turn;
-		}
-	});
-});
